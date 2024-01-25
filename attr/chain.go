ec1244e975afba9a7b7bf1c7e5500a490fd8d822
thisisaaronland <devnull@localhost>
2024-01-24 23:10:17 -0800

    simplify signature for ReplaceAttrChain

--- attr/chain.go
+++ attr/chain.go
@@ -4,7 +4,7 @@ import (
 	"log/slog"
 )
 
-func ReplaceAttrChain(chain []func(groups []string, a slog.Attr) slog.Attr) func(groups []string, a slog.Attr) slog.Attr {
+func ReplaceAttrChain(chain ...func(groups []string, a slog.Attr) slog.Attr) func(groups []string, a slog.Attr) slog.Attr {
 
 	fn := func(groups []string, a slog.Attr) slog.Attr {
 
--- attr/chain_test.go
+++ attr/chain_test.go
@@ -9,9 +9,9 @@ import (
 
 func TestReplaceAttrChain(t *testing.T) {
 
-	replace_attr := ReplaceAttrChain([]func(groups []string, a slog.Attr) slog.Attr{
+	replace_attr := ReplaceAttrChain(
 		EmojiLevelFunc(),
-	})
+	)
 
 	th := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		Level:       LevelTrace,
